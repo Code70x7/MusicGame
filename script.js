f0c0ed10e1cdbdae00c07a5c28b4789c3ee027a2
Tony Dayton <tonylynndayton@yahoo.com>
2023-08-17 00:20:35 -0600

    Added music

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myDiv = document.getElementById('myDiv');
+const audio = new Audio('assets/gamemusic.mp3'); // Replace 'sound.mp3' with your actual sound file path
 
 myDiv.addEventListener('click', () => {
     myDiv.style.display = 'none';
+    audio.play(); // Play the audio file
 });
