316e8902ae79fa9c990c97c68e3e6ee8a782509a
Tony Dayton <tonylynndayton@yahoo.com>
2023-08-26 00:30:02 -0600

    volume change

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ document.getElementById("timer").innerHTML = `
     <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <g class="base-timer__circle">
         <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-        <path
+        <path>
             id="base-timer-path-remaining"
             stroke-dasharray="283"
             class="base-timer__path-remaining ${remainingPathColor}"
@@ -92,7 +92,7 @@ document.getElementById("timer").innerHTML = `
             a 45,45 0 1,0 90,0
             a 45,45 0 1,0 -90,0
             "
-        ></path>
+        </path>
         </g>
     </svg>
     <span id="base-timer-label" class="base-timer__label">${formatTime(
@@ -267,6 +267,10 @@ function playGame() {
 
   document.getElementById("body").addEventListener("keydown", (event) => {
     console.log(matchSeq); // Debugging code; to be deleted
+
+    const audioElement = document.getElementById(audioMap.get(matchSeq[0]));
+    audioElement.volume = 0.1; // Adjust the volume here
+    audioElement.play();
 
     if (event.key == matchSeq[0]) {
       document.getElementById(audioMap.get(matchSeq[0])).play();
